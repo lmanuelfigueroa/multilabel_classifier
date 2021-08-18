5b33f4148f5b4fda669ff806619889ca76e17917
Luis M <lmanuelfigueroa@gmail.com>
2021-08-17 22:01:16 -0700

    completed search docuent feature

--- pages/search_corpus.py
+++ pages/search_corpus.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from spacy.lang.en import English
 import operator
-import spacy
-import glob
+from csv import reader
 import pandas as pd
+import ast
 
 tags = ['aid', 'american', 'army', 'buildings', 'camp', 'family', 'fear', 'food', 
 'ghetto', 'government', 'hiding', 'hospital', 'hunger', 'Jewish', 'kill','Nazi', 'police', 'polish', 'prisoners', 'religion', 
 'relocation', 'russian', 'school', 'shooting', 'sick', 'survive', 'synagogue', 'war', 'work']
-
 
 
 screen_tags = ['','aid', 'american','army', 'hunger','war' ,'buildings', 'work', 'camp', 'family', 'fear', 'food', 
@@ -19,147 +18,73 @@ def app() -> None:
     st.title("Search Corpus")
     main = st.form("current")
     selected_tag = main.selectbox("Select Tag to Search Documents",screen_tags)
-    percentage = main.number_input("Enter a percentage: ex: 50",min_value = 0, max_value = 100, value = 50)
+    top_number = main.number_input("Enter a number: ",min_value = 1, max_value = 10, value = 3)
     submit_button = main.form_submit_button("Submit")
     if submit_button:
-        load_model(selected_tag,percentage)
+        read_model_results(selected_tag,top_number)
 
 
 
-def load_model(tag, percentage) -> None:
+def read_model_results(tag, top_number) -> None:
     
-    nlp = English()
+    csv_path = "csv_files/all_tags.csv"
 
-    #path = "single_test_reading/*.txt"
-    path = "testing_multilabel_corpus_reading/*.txt"
-    #path = "new_ocr/*.txt"
-    files = glob.glob(str(path))
-    rn_numbers = []
-    result_tags = []
+    documents = []
+    with open(csv_path,"r") as line:
+        row = reader(line)
 
-    for file in files:
+        #removing the first line which is the column names 
+        next(row)
 
-        reference = file.split("\\")[1].replace(".txt","")
-        rg_num = reference.split("_")[0]
-        reference = reference +".pdf#page6"
-        url = f"https://collections.ushmm.org/oh_findingaids/{reference}"
-        transcript = f'<a target="_blank" onclick="find({rg_num});" href="{url}">{rg_num}</a>' 
+        #iterating the csv files and coverting the string values to strings for document RG and tuples for tag percentages
+        for col in row:
+            documents.append((col[1],ast.literal_eval(col[2])))
 
+    reduced_tag_percentages = reduce_tag_results(documents,top_number)
+
+    results = []
+    rg_links = []
+
+
+    # reference = file.split("\\")[1].replace(".txt","")
+    # rg_num = reference.split("_")[0]
+    # reference = reference +".pdf#page6"
+    # url = f"https://collections.ushmm.org/oh_findingaids/{reference}"
+    # transcript = f'<a target="_blank" onclick="find({rg_num});" href="{url}">{rg_num}</a>' 
+
+    #iterating through the list and searching for the documents where the tap is in the top n tag percentages
+    for doc in reduced_tag_percentages:
+
+        #creating the links to the documents using the rg numbers
+        doc_link = doc[0] + "_trs_en.pdf#page6"
+        url = f"https://collections.ushmm.org/oh_findingaids/{doc_link}"
+        link = f'<a target="_blank" onclick="find({doc[0]});" href="{url}">{doc[0]}</a>' 
         
-        with open(file, "r",encoding= "utf-8") as f:
-            data = f.read()
-            top_3_labels = process_document(nlp,data,percentage)
-            #st.write(top_3_labels)
-            print(top_3_labels)
-            if top_3_labels[0][0] == tag:
-                rn_numbers.append(transcript)
-                result_tags.append(top_3_labels)
 
-    
-    table_data = pd.DataFrame({'Testimony':rn_numbers,'Top Tags':result_tags})
+        for i in range(top_number):
+            if doc[1][i][0] == tag:
+                rg_links.append(link)
+                results.append(doc[1])
+                
+    table_data = pd.DataFrame({'Testimony':rg_links,'Top Tags':results})
     table_data = table_data.to_html(escape = False)
     st.write(table_data,unsafe_allow_html = True)
 
 
-def process_document(nlp,data,percentage) -> list:
+def reduce_tag_results(documents, top_number):
 
-    PERCENTAGE_CONSIDER_TAG = percentage/100
-    label_dict = dict.fromkeys(tags,0)
-    nlp_labels = spacy.load("model_output_exclusive/model-last")
+    small_doc_list = []
+        
+    #iterating through the tag percentages of each list 
+    for item in documents:
+        dict_pairs = item[1].items()
+        pair_iter =  iter(dict_pairs)
+        temp = []
+        
+        #geting the top n tags asked by the user
+        for i in range(top_number):
+            pair = next(pair_iter)
+            temp.append(pair)
+        small_doc_list.append((item[0],temp))
     
-    if nlp.pipe_names != []:
-        nlp.remove_pipe("sentencizer")
-
-    nlp.max_length = 174214790
-    nlp.add_pipe('sentencizer')
-    doc = nlp(data, disable=['parser', 'tagger', 'ner'])
-
-    num_of_sentences = len(list(doc.sents))
-
-    num_of_tags_found = 0
-    for sent in doc.sents:
-        text = remove_accents(str(sent.text))
-        doc = nlp_labels(text)
-    
-        first_tag = top_tag(doc.cats)
-        if doc.cats[first_tag] > PERCENTAGE_CONSIDER_TAG:
-            num_of_tags_found +=1
-            label_dict[first_tag] = label_dict[first_tag] + 1
-                
-    st.write(f"The documents contains {num_of_sentences} sentences and {num_of_tags_found} tags were assigned on {round(PERCENTAGE_CONSIDER_TAG*100)}% tag requirement\n")
-
-    #getting the percentage of tags for the document
-    for tag in label_dict:
-        label_dict[tag] = round(label_dict[tag]*100/num_of_tags_found)
-
-
-    sorted_d = dict( sorted(label_dict.items(), key=operator.itemgetter(1),reverse=True))
-    dict_pairs = sorted_d.items()
-    pair_iter =  iter(dict_pairs)
-
-    top_tags = []
-
-    NUM_TOP_TAGS = 3
-    
-    for i in range(NUM_TOP_TAGS):
-        pair = next(pair_iter)
-        top_tags.append(pair)
-    
-    return top_tags
-
-def print_top_tags(tags: dict) -> None:
-
-    NUMB_OF_TAGS = 3
-    sorted_d = dict( sorted(tags.items(), key=operator.itemgetter(1),reverse=True))
-    dict_pairs = sorted_d.items()
-    pair_iter =  iter(dict_pairs)
-
-    top_tags = []
-    
-    for i in range(NUMB_OF_TAGS):
-        pair = next(pair_iter)
-        top_tags.append(pair)
-    
-    st.write("\n")
-    st.write(f"The top 3 most common tags in the document are {top_tags}")
-
-
-def top_tag(tags : dict) -> str:
-    sorted_d = dict( sorted(tags.items(), key=operator.itemgetter(1),reverse=True))
-    dict_pairs = sorted_d.items()
-    pair_iter =  iter(dict_pairs)
-    pair = next(pair_iter)
-    return pair[0]
-
-def remove_accents(text):
-    #Polish letters
-    letters= {
-    'ł':'l', 'ą':'a', 'ń':'n', 'ć':'c', 'ó':'o', 'ę':'e', 'ś':'s', 'ź':'z', 'ż':'z',
-    'Ł':'L', 'Ą':'A', 'Ń':'N', 'Ć':'C', 'Ó':'O', 'Ę':'E', 'Ś':'S', 'Ź':'Z', 'Ż':'Z',
-
-    #Accent Vowels
-    "à":"a", "á":"a", "â":"a", "ã":"a", "ä":"a", "å":"a", "æ": "ae",
-    "À":"A", "Á":"A", "Â":"A", "Ã":"A", "Ä":"A", "Å":"A", "Æ": "ae",
-
-    "è":"e", "é":"e", "ê":"e", "ë":"e",
-    "È":"E", "É":"E", "Ê":"E", "Ë":"E",
-
-    "ì":"i", "í":"i", "î":"i", "ï":"i",
-    "Ì":"I", "Í":"I", "Î":"I", "Ï":"I",
-
-    "ò": "o", "ó": "o", "ô": "o",  "õ": "o",  "ö": "o", "ø": "o",
-    "Ò": "O", "Ó": "O", "Ô": "O",  "Õ": "O",  "Ö": "O", "Ø": "O",
-
-    "ù": "u", "ú": "u",  "û": "u",  "ü": "u",
-    "Ù": "U", "Ú": "U",  "Û": "U",  "Ü": "U",
-
-    "ý": "y", "ÿ": "y",
-    "Ý": "Y", "Ÿ": "Y",
-
-    #Accent Cononants
-    "ç": "c", "Ç": "C",
-    "ß": "ss"
-    }
-    trans=str.maketrans(letters)
-    result=text.translate(trans)
-    return (result)
+    return  small_doc_list
--- testing_app.py
+++ testing_app.py
@@ -1,9 +1,10 @@
 from os import link
+from numpy import TooHardError
 import streamlit as st
 import glob
-import requests 
-from bs4 import BeautifulSoup
-
+from csv import reader
+import ast
+import operator
 
 
 tags = ['aid', 'american','army', 'hunger','war' ,'buildings', 'work', 'camp', 'family', 'fear', 'food', 
@@ -12,34 +13,77 @@ tags = ['aid', 'american','army', 'hunger','war' ,'buildings', 'work', 'camp', '
 
 
 def display_app():
+
+    references = []
     path = "new_ocr/*txt"
+    TOP_NUMBER = 3
+    csv_path = "csv_files/all_tags.csv"
+    tag = "buildings"
 
-    files = glob.glob(str(path))
+    with open(csv_path,"r") as line:
+        row = reader(line)
 
-    transcripts = []
-    dic = {"one":1 , "two":0, "three":4}
+        next(row)
+        for col in row:
+            references.append((col[1],ast.literal_eval(col[2])))
     
-
+    #for line in references:
+        #print(line[1])
+        
+    #print(references)
+    smaller = reduce_dict(references,TOP_NUMBER,tag)
+    
+    results = []
     count = 0
-    for file in files:
+    print(len(smaller))
+    for reference in smaller:
+        #print(reference)
+        for i in range(TOP_NUMBER):
+            if reference[1][i][0] == tag:
+                results.append(reference)
+                
+    print(len(results))
+    print(results)
+
+
+def reduce_dict(reference,limit,tag):
         
-        temp = file.split("\\")[1].replace(".txt","")
-        rg_num = temp.split("_")[0]
-        temp = temp +".pdf#page6"
-        url = f"https://collections.ushmm.org/oh_findingaids/{temp}"
-        transcript = f'<a target="_blank" onclick="find({rg_num});" href="{url}">{rg_num}</a>' 
-        transcripts.append(url)
-        # with open(file, "r",encoding= "utf-8") as f:
-        #     data = f.read()
+    
+    small_reference = []
+        #sorted_d = dict( sorted(tags.items(), key=operator.itemgetter(1),reverse=True))
+    for item in reference:
+        dict_pairs = item[1].items()
+        pair_iter =  iter(dict_pairs)
+        small_dict = []
+        #print(item)
+        for i in range(limit):
+            pair = next(pair_iter)
+            small_dict.append(pair)
+            # if tag == pair[0]:
+            #     small_dict.append(pair)
+        small_reference.append((item[0],small_dict))
+    
+    return small_reference
+    # count = 0
+    # for file in files:
         
-    links = [] 
-    for items in transcripts:
-        reqs = requests.get(items)
-        soup = BeautifulSoup(reqs.text, "html.parser")
-        element = str(soup.find_all('title'))
-        if "Not Found" in element:
+    #     temp = file.split("\\")[1].replace(".txt","")
+    #     rg_num = temp.split("_")[0]
+    #     temp = temp +".pdf#page6"
+    #     url = f"https://collections.ushmm.org/oh_findingaids/{temp}"
+    #     transcript = f'<a target="_blank" onclick="find({rg_num});" href="{url}">{rg_num}</a>' 
+    #     transcripts.append(url)
+    #     # with open(file, "r",encoding= "utf-8") as f:
+    #     #     data = f.read()
+        
+    # links = [] 
+    # for items in transcripts:
+    #     reqs = requests.get(items)
+    #     soup = BeautifulSoup(reqs.text, "html.parser")
+    #     element = str(soup.find_all('title'))
+    #     if "Not Found" in element:
            
-            links.append(items)
+    #         links.append(items)
 
         # for title in soup.find_all("title"):
         #     print(title.get_text())
